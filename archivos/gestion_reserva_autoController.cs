4da2642de370fe20ccbd713ec36cbcc0b63a42e4
anagduran <agabrieladuran1@gmail.com>
2016-12-10 00:39:20 -0400

    Add files via upload

---
+++ archivos/gestion_reserva_autoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FOReserva.Models.Autos;
+
+namespace FOReserva.Controllers
+{
+    public class gestion_reserva_autoController : Controller
+    {
+        //
+        // GET: /gestion_reserva_autos/
+
+        public ActionResult M19_Reserva_Autos()
+        {
+            Cvista_ReservaAutos model = new Cvista_ReservaAutos();
+            return PartialView(model);
+        }
+
+        public ActionResult M19_Reserva_AutosImagenes()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public JsonResult buscarCarro(Cvista_ReservaAutos model)
+        {
+            int sel = model.SelectedCiudadIdOrigen;
+            int sel2 = model.SelectedCiudadIdDestino;
+
+            //Console.WriteLine(prueba);
+            
+            return (Json(true, JsonRequestBehavior.AllowGet));
+        }
+
+
+    }
+}
